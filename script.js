8586f4944457359ee01b02f61917f636ee2aa781
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-10-15 16:25:07 +0100

    Logic for modal

---
+++ script.js
@@ -0,0 +1,53 @@
+// var heightPage = document.querySelector('.page-container').offsetHeight;
+// console.log(heightPage);
+
+// document.querySelector('.highlights-modal').style.height =
+//   document.querySelector('.page-container').offsetHeight;
+
+// var heightModal = document.querySelector('.highlights-modal').offsetHeight;
+// console.log(heightModal);
+
+function showModal(targetModal) {
+  // Shows modal according to the title of the element, which was clicked
+  let modalTitle = targetModal.getAttribute('title');
+
+  let modalContainer = document.getElementById(`${modalTitle}-modal-container`);
+  modalContainer.classList.add('modal-container');
+  console.log(modalTitle);
+  console.log(modalContainer);
+  console.log(modalContainer.classList);
+
+  // Call close modal function when user tap on modal container
+  modalContainer.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target === modalContainer) {
+      closeModal();
+    }
+  });
+}
+
+// Click the button, call modal with corresponding title
+let showModalButton = document.querySelectorAll('a.info-block');
+
+showModalButton.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    let targetModal = event.target;
+
+    showModal(targetModal);
+  });
+});
+
+// Function closes modal
+function closeModal() {
+  let modalContainer = document.querySelector('.modal-container');
+  modalContainer.classList.remove('modal-container');
+}
+
+// Call close modal function when user press Esc button on keyboard
+window.addEventListener('keydown', (event) => {
+  let modalContainer = document.querySelector('.modal-container');
+  if (modalContainer && event.key === 'Escape') {
+    closeModal();
+  }
+});
