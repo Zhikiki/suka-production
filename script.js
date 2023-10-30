2f90699fbff57caa10b0c8bdba35c55b958b70aa
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-10-30 10:59:35 +0000

    Styled carousel indicators for small scr

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ let showModalButtons = document.querySelectorAll('a.info-block');
 
 showModalButtons.forEach((item) => {
   item.addEventListener('click', (event) => {
+    event.preventDefault();
     let targetModal = event.target.getAttribute('title');
     showModal(targetModal);
   });
